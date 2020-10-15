db6f6d1dc9c5228c1c5fa0ca7b1d037b5a34b1c2
Francisco Edilton <franciscoed@gmail.com>
2020-10-15 10:18:05 +0100

    Live da Rede 100

---
+++ livedarede100/cloudtrail.py
@@ -0,0 +1,28 @@
+import boto3
+import json
+
+snsARN = 'arn:aws:sns:us-east-1:167798398842:Lambda'
+
+def lambda_handler(event, context):
+	eventname = event['detail']['eventName']
+	snsclient = boto3.client('sns')
+	if (eventname == 'StopLogging'):
+	    cloudtrailArn= event['detail']['requestParameters']['name']
+	    snspublish = snsclient.publish(
+	                     TargetArn = snsARN,
+	                     Subject=("INCIDENTE DE SEGURANÇA - Cloudtrail."),
+	                     Message=json.dumps({'default': json.dumps(event)}),
+	                     MessageStructure='json')
+	    
+	    try:
+	        client = boto3.client('cloudtrail')
+	        enablelogging = client.start_logging(Name=cloudtrailArn)
+	    except:
+	        print("Erro ativando Cloudtrail")
+	else:
+		snspublish = snsclient.publish(
+						TargetArn= snsARN,
+						Subject=("CloudTrail ativado com sucesso"),
+						Message=json.dumps({'default': json.dumps(event)}),
+						MessageStructure='json')
+
